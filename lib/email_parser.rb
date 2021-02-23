9fb7107fd128c7bb4b1e9289dc788b288c2eeb05
Emily Nguyen <emilyn0614@gmail.com>
2021-02-23 14:39:19 -0600

    commit tree 81e627d13c105de7488647f7ee99c85760a809b1

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,13 +3,14 @@
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
 
+#require 'pry'
 class EmailAddressParser
     attr_accessor :emails
     def initialize(emails)
         @emails = emails
     end
     def parse
+#binding.pry
         emails.split(/, | /).uniq
-        # ' , '  ', ' '/,/'
     end
 end
